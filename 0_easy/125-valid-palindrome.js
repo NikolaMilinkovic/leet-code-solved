fb7d4e6506844332182c3608d9c650d17f55efc4
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2024-07-10 01:20:11 +0200

    Update 125-valid-palindrome file

--- 0_easy/125-valid-palindrome.js
+++ 0_easy/125-valid-palindrome.js
@@ -14,3 +14,10 @@ const isPalindrome = (string) => {
   if(processedString === reversedString) return true;
   return false;
 };
+
+// Less verbose version / Lower performance for some reason on LeetCode
+// But overall should be more performant than previous method
+const isPalindrome2 = (string) => {
+  const cleanedStr = string.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  return cleanedStr === cleanedStr.split('').reverse().join('');
+};
