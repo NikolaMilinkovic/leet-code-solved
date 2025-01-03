e8dba9ee54535d8977461fc4fcb5292dc2999834
Nikola Milinkovic <nikolamilinkovic221@gmail.com>
2025-01-03 15:14:45 +0100

    518-coin-change-II

---
+++ 1_medium/518-coin-change-II.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+function change (amount, coins) {
+  let numsOfWays = Array(amount + 1).fill(0);
+  numsOfWays[0] = 1;
+  for(const coin of coins){
+    for(let currAmount = coin; currAmount <= amount; ++currAmount){
+      console.log(`> Logging num of ways before update: ${numsOfWays}`);
+      numsOfWays[currAmount] += numsOfWays[currAmount - coin];
+      console.log(`> Logging num of ways AFTER update: ${numsOfWays}`);
+    }
+  }
+
+  return numsOfWays.pop();
+};
